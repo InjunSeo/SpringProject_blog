a2bec2978a791205e4c9da639a46888494b92b86
injunSeo <fregesaxiom@gmail.com>
2021-05-31 18:17:25 +0900

    EssayService.java
    EssayServiceImpl.java

---
+++ src/main/java/SpringProject00/demo/service/EssayService.java
@@ -0,0 +1,25 @@
+package SpringProject00.demo.service;
+
+import SpringProject00.demo.domain.Essay;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EssayService {
+    Essay write(Essay essay);
+
+    Essay edit(Essay essay);
+
+    Essay delete(Essay essay);
+
+    Essay countFavour(Essay essay);
+
+    Optional<Essay> findById(long id);
+
+    Optional<Essay> findByName(String writer);
+
+    Optional<Essay> findByTitle(String title);
+
+    List<Essay> findAll();
+
+}
---
+++ src/main/java/SpringProject00/demo/service/EssayServiceImpl.java
@@ -0,0 +1,60 @@
+package SpringProject00.demo.service;
+
+import SpringProject00.demo.domain.Essay;
+import SpringProject00.demo.repository.EssayRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EssayServiceImpl implements EssayService {
+    private final EssayRepository essayRepository;
+
+    public EssayServiceImpl(EssayRepository essayRepository) {
+        this.essayRepository = essayRepository;
+    }
+
+    @Override
+    public Essay write(Essay essay) {
+        Essay save = essayRepository.save(essay);
+        System.out.println("New essay = " + essay);
+        return save;
+    }
+
+    @Override
+    public Essay edit(Essay essay) {
+        Essay modify = essayRepository.modify(essay);
+        return modify;
+    }
+
+    @Override
+    public Essay delete(Essay essay) {
+        Essay delete = essayRepository.delete(essay);
+        return delete;
+    }
+
+    @Override
+    public Essay countFavour(Essay essay) {
+        return essayRepository.countFavour(essay);
+    }
+
+    @Override
+    public Optional<Essay> findById(long id) {
+        Optional<Essay> essay = essayRepository.findById(id);
+        return essay;
+    }
+
+    @Override
+    public Optional<Essay> findByName(String writer) {
+        return essayRepository.findByName(writer);
+    }
+
+    @Override
+    public Optional<Essay> findByTitle(String title) {
+        return essayRepository.findByTitle(title);
+    }
+
+    @Override
+    public List<Essay> findAll() {
+        return essayRepository.findAll();
+    }
+}
