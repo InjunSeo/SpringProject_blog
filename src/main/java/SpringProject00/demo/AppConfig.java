6fb2e78ef9cf0c34dc31c78b574ea893b5739930
injunSeo <fregesaxiom@gmail.com>
2021-06-13 13:27:06 +0900

    AppConfig.java
    DataSource 추가, Essayservice에서 JdbcTemplateEssayRepository로 변경

--- src/main/java/SpringProject00/demo/AppConfig.java
+++ src/main/java/SpringProject00/demo/AppConfig.java
@@ -1,5 +1,6 @@
 package SpringProject00.demo;
 
+import SpringProject00.demo.repository.JdbcTemplateEssayRepository;
 import SpringProject00.demo.repository.MemoryEssayRepository;
 import SpringProject00.demo.repository.MemoryFavouringRepository;
 import SpringProject00.demo.repository.MemoryMemberRepository;
@@ -7,8 +8,15 @@ import SpringProject00.demo.service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class AppConfig {
+    private final DataSource dataSource;
+
+    public AppConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,7 +25,7 @@ public class AppConfig {
 
     @Bean
     public EssayService essayService() {
-        return new EssayServiceImpl(new MemoryEssayRepository());
+        return new EssayServiceImpl(new JdbcTemplateEssayRepository(dataSource));
     }
 
     @Bean
