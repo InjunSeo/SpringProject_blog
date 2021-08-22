0efc017105aee5b6c001c8aaa38acd4d083b47c8
injunSeo <fregesaxiom@gmail.com>
2021-08-22 15:40:47 +0900

    @Valid 추가

--- src/main/java/SpringProject00/demo/controller/MemberController.java
+++ src/main/java/SpringProject00/demo/controller/MemberController.java
@@ -3,27 +3,33 @@ package SpringProject00.demo.controller;
 import SpringProject00.demo.domain.Member;
 import SpringProject00.demo.service.MemberService;
 import SpringProject00.demo.service.MemberServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
     private final MemberService memberService;
 
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
     @GetMapping(value = "/members/new")
-    public String createForm() {
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
     @PostMapping(value = "/members/new")
-    public String create(MemberForm form) {
+    public String create(@Valid MemberForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/createMemberForm";
+        }
         Member member = new Member();
         member.setId(form.getId());
         member.setName(form.getName());
@@ -31,4 +37,6 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+   
 }
