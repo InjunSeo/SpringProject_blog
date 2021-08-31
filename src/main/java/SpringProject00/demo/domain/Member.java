0826acc9738e35c041d39a35c243862d8a4ed4a9
injunSeo <fregesaxiom@gmail.com>
2021-08-31 19:20:31 +0900

    @Column name 추가

--- src/main/java/SpringProject00/demo/domain/Member.java
+++ src/main/java/SpringProject00/demo/domain/Member.java
@@ -3,10 +3,7 @@ package SpringProject00.demo.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -14,9 +11,13 @@ import java.util.Date;
 @Getter @Setter
 public class Member {
     @Id
+    @Column(name = "MEMBER_ID")
     private String id;
+
     private String name;
+
     private LocalDateTime createDate;
+
     private char email;
 
 
