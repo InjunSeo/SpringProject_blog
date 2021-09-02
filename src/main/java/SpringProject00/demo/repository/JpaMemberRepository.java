fbe72cd4281ef7ff713b19d51ef07f21a113e55f
injunSeo <fregesaxiom@gmail.com>
2021-09-02 19:19:38 +0900

    repository 추가

--- src/main/java/SpringProject00/demo/repository/JpaMemberRepository.java
+++ src/main/java/SpringProject00/demo/repository/JpaMemberRepository.java
@@ -1,2 +1,40 @@
-package SpringProject00.demo.repository;public class JpaMemberRepository {
+package SpringProject00.demo.repository;
+
+import SpringProject00.demo.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+@Primary
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        Member member = em.find(Member.class, name);
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
 }
